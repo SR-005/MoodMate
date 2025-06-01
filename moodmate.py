1f3f71e3330a3d7fda26f2e4c936c1989184a056
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-06-01 21:13:15 +0530

    update

--- moodmate.py
+++ moodmate.py
@@ -1,6 +1,26 @@
 import customtkinter as ctk
+from tkinter import font
+from datetime import datetime
+ctk.set_appearance_mode("dark")  
+ctk.set_default_color_theme("blue")  
 
-app=ctk.CTk()
-app.geometry("1080x720")
+main=ctk.CTk()
+main.geometry("800x800")
 
-app.mainloop()
+#FONT FAMILIES
+font1=ctk.CTkFont(family="Coolvetica",size=12)
+        #font1=font.Font(family="Coolvetica Rg",size=12)
+font2=ctk.CTkFont(family="EastmanRomanTrial-Medium",size=12)
+
+
+heading=ctk.CTkLabel(main,text="MoodMate",font=("Coolvetica",65),width=125,height=25,text_color="#096fb3")
+heading.place(x=255,y=20)
+
+currentdate=datetime.today().strftime('%d-%m-%Y')
+datelabel=ctk.CTkLabel(main,text="Date: "+currentdate,font=("Coolvetica",20))
+datelabel.place(x=320,y=87)
+
+welcomelabel=ctk.CTkLabel(main,text="Hello User!! Hope your having a GREAT DAY",font=("EastmanRomanTrial-Medium",20))
+welcomelabel.place(x=200,y=200)
+
+main.mainloop()
